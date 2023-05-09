681d9fed3ed3488797e45fc72d5b73d96ff123a2
Temur <temur.chichua@iliauni.edu.ge>
2023-05-09 21:08:59 +0400

    [LeetCode] 54. Spiral Matrix

---
+++ LeetCode/Array/String/54. Spiral Matrix.py
@@ -0,0 +1,28 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        m = len(matrix)
+        n = len(matrix[0])
+        size = m * n
+        res = []
+
+        m_start, m_end = 0, m - 1
+        n_start, n_end = 0, n - 1
+
+        while m_start <= m_end and n_start <= n_end:
+            for j in range(n_start, n_end + 1):
+                res.append(matrix[m_start][j])
+            m_start += 1
+
+            for i in range(m_start, m_end + 1):
+                res.append(matrix[i][n_end])
+            n_end -= 1
+
+            for j in range(n_end, n_start - 1, -1):
+                res.append(matrix[m_end][j])
+            m_end -= 1
+
+            for i in range(m_end, m_start - 1, -1):
+                res.append(matrix[i][n_start])
+            n_start += 1
+
+        return res[:size]
